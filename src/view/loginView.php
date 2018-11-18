d593e33e22fea87620392b1dd4758d22e024cabe
Daniel Chung <danielvchung@hotmail.com>
2018-11-18 05:59:35 -0200

    Tela de login Desk e Mobile criado

--- src/view/loginView.php
+++ src/view/loginView.php
@@ -8,8 +8,9 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="icon" href="favicon.png" type="image/x-icon">
 	<link rel="stylesheet" href="app/css/reset.css">
-	<link rel="stylesheet" href="app/css/login/login.css">
-	<link rel="stylesheet" href="app/css/login/login-responsive.css">
+	<link rel="stylesheet" href="app/css/login/login-desk.css">
+	<link rel="stylesheet" href="app/css/login/login-tablet.css">    
+	<link rel="stylesheet" href="app/css/login/login-mobile.css">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 </head>
 <body>
@@ -24,7 +25,7 @@
 				<tbody class="table-body">
 					<tr>
 						<td class="cel-name cel-icon">
-							<input class="name input-icon" type="text" name="name" placeholder="Usuário" pattern="[a-zA-Z]{4,}" title="Mínimo 4 letras" required>
+							<input class="name input-icon" type="text" name="name" placeholder="Usuário" pattern="[a-zA-Z]{4,}" title="Mínimo 4 letras" autocomplete="off" required>
 							<i class="fas fa-user"></i>
 						</td>
 					</tr>
